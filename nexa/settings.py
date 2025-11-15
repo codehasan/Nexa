08f6f2466b2c7072359f84f52a726d734e4d0ece
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2025-11-15 15:41:00 +0600

    reduce token lifetime

--- nexa/settings.py
+++ nexa/settings.py
@@ -152,8 +152,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("JWT",),
 }
 
